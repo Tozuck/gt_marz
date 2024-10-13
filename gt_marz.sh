827258459dd30db0f7b9243800a99fda56728081
Tozuck <122993981+Tozuck@users.noreply.github.com>
2024-10-13 17:39:57 +0330

    Update gt_marz.sh

--- gt_marz.sh
+++ gt_marz.sh
@@ -18,12 +18,17 @@ sudo git clone https://github.com/Gozargah/Marzban-node
 cat <<EOL > ~/Marzban-node/docker-compose.yml
 services:
   marzban-node:
+    # build: .
     image: gozargah/marzban-node:latest
     restart: always
     network_mode: host
 
+    # env_file: .env
     environment:
+      SSL_CERT_FILE: "/var/lib/marzban-node/ssl_cert.pem"
+      SSL_KEY_FILE: "/var/lib/marzban-node/ssl_key.pem"
       SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/ssl_client_cert.pem"
+      SERVICE_PROTOCOL: "rest"
 
     volumes:
       - /var/lib/marzban-node:/var/lib/marzban-node
@@ -33,30 +38,30 @@ EOL
 cat <<EOL > /var/lib/marzban-node/ssl_client_cert.pem
 -----BEGIN CERTIFICATE-----
 MIIEnDCCAoQCAQAwDQYJKoZIhvcNAQENBQAwEzERMA8GA1UEAwwIR296YXJnYWgw
-IBcNMjQwNzI0MTAwNTUzWhgPMjEyNDA2MzAxMDA1NTNaMBMxETAPBgNVBAMMCEdv
-emFyZ2FoMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAyrRzCGIBjVC/
-7b1phU5sWoEuj44KZRQs/eUJAHWvlpHMw3YHLwaNyJomzVboBOXrKqTzZlpU3BTG
-jB+fUucthVA6lA/e/fx+gx4nNs8crlCN0DikrBee1IiYYr2bKuPc0zmn93FYy5Tv
-xHcaDMRVq4YgR5nDGk6blt96kb16sWrDBbHb86jwDZUUZcWXQL+p9bNI8nody92K
-EjUWoFC0eXVVdIb9hStUypo9VNaRRqU3PChzVQIhSoavedjV29D5ghj3VsW0/hjz
-T64Kb8OjdiQCTxTuiGWSxVb0p5kJXve8nyiLBSay8UtmpiwcR3gf61qQW8sZBy7x
-Z5h3QaFdzgh1me8NtbNsE0rGP2z9uZUn5Vnk57kEi3dp+2W8AVd6OyGg+9k/2Q60
-9tG2fG+pejgWyjj0ho0LpbQnwOfrrs/3v4uNhWTH1EQiUKMaIM/t/Cco3I3PsM7r
-NM7IHYDm1LcW5bmejXsnae0WKNPLiZT6fpFjVe4r+3d4sEVyPCzNuKEYJaeYqGkr
-RjE3mmkIMTynYfMQPLZvYIY1oOVwEGtQkvDkyWi5JpzwTC0yKQ6W1aN89eSmUXH3
-9rnw1RVMzYFpGs0JqY+W5+ZiFWlmOwcjYJkhsp62hKMdgt8zUMhi188LIkO1iA13
-B/vfHlplxbC22gzLNn9iRWVWYH+dKiECAwEAATANBgkqhkiG9w0BAQ0FAAOCAgEA
-Q02iZjPcak768yfTZR3o0WKi/jxwqJPiTch/tNPPEctrTqbcTTORS72zA6dEVGop
-3Ro8sEVsiUgafwaun2+ZXtIl2cQ8MKMMws6/siOnPbfREN8km3CAV7MQf12IEPPp
-q2QK1jFGYAjHa6egTz2ChJ41xRUw/i5BxZRSSwYigDWv8ZVFrn944NUktviJLmCU
-8hQSokysOe9r/8zu4vwGqQLX6fdJ+KxbUvyOZSeGEpQf6HMGdwkK5eK1Xr+8vSCV
-fIGrOs0GHAhGF5lrnATj8nya+ZR2wLKziaFOiNstQJOsd5sqQeLwvtLgymKoOMng
-r6AVnxV9Bm+5Las2ihNK0y58W4d1+QiWKg2k/zWx6CJJ+1XnCtjw4LDySuJAux2X
-luh/ikXp17QH5w1gLBXVIn3JCrSbNlPKZ3ctZ+MUhEabqwngrYvHPMwK9XqQd4t1
-c7lApX4fx6hwf8k8rsCd1v4RBuS6fWETPrgRQn+uecYJbajGZAiCVz5xYYIL5efV
-J5ric+qzLJyf884+YaVMwo47P2LWa/yVRW83T2jkhskdGZXAwH3pPiZ4CFWhk1Dc
-IWdhvApGPpN4kB9aCEyNeOZYHTwUcuh2rRbAkQNNX4Ou48YCOJulzVWI3oI8mw7W
-TaE+WUoPeD4XChJXeOtpUOYWvrz+wDTd64wLMrzsaWE=
+IBcNMjQxMDEzMTQwMjI2WhgPMjEyNDA5MTkxNDAyMjZaMBMxETAPBgNVBAMMCEdv
+emFyZ2FoMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEArmf8GVX4coVz
+1D9e2OtvpSyaaI0cbnNOGUpfoOBwAJtRrWH3jPCyb7BnglKplCO1G1rHkeiR/tUA
+PDm09oJpHSs/t5C34N1UUuw+vYdJrx2KY043FUhb7xwrDFA93wXkUtyZp752IOL3
+vZPZBX2Cc1GelQNEtgpVklLM3R4RzlpjC6TMaJ16MJgQGq1byNjhv5kKN7XsJbus
+NN+qx+NBvaBApAvtaqtNPXhoAvWP/69uGkcSQIY2kiDYCC+ACJP50z3+uCf5gY1e
+K0SgXZ2EwmR9nUPyrDc+znb0tbWYC6Q06WOloJfMOgy+0nzegj7yf72MZnfcObKj
+FAxYLG6C1sjd8M+3asEE7eFyNbaC546BwCoX98Jqtckrxh5NgYfiFPRKW4GdcJzU
+Dmc4EjDtSdJv30H15f22gw6Ltj/VWT3cjVnNWfgLQdv5/8EtYBNjJOVUYAz5C+IY
+V2KtYoYqNVahD5dP7v1q7NfYmbmw+Go/crnaPDQCb89kCcn4yWqOnTN4+fClf8XV
+MH27NWGDmJ/TQcpKDefh+xel56R8bNB3C11Wb62kYrP9NKxMPgt8ETRFYVv+fUWt
+mTBbeKSMERNhpJJeJ9uXzxx9F2Alb0AbDYDvK3RxPsnywUV4aQtLT/C0epMunV6s
+MmtDa9Tb8INFccQacl8z9T2ZMQL3YzsCAwEAATANBgkqhkiG9w0BAQ0FAAOCAgEA
+Bqw6UsOghAHau0/yvo/mAFV1UULHIzchiRd1IQ8LIsZepDxIAn1kCyFkiRaLYs8K
+pCA/WJWXIXtQUNc3zmZGaWGkUGe+euI6MOmeI/ok9vEpy0ZnP5yYwZky7UgD+xkq
+aRT1aJGMh8rerBByjBQA9gLFOjireLWPLDV2EnbHmN4Ob2ac7SzQoD/zRv5UezwA
+hL/LC6BHydQRac6tnb+5khJoYeupb2sz+ipNW1Dgf1wggjqyuj+nWT18932+nU1q
+CGogDQehg94eOrhcVXhs8TR/OttlPx0CFNyEyLF+k3yZ+aPLwqH98/CzkUiRflgk
+UeL6JuI7maZxUx4Z7zmz941Pbr3RNjWI3Xpkmp2QvzkrjITsO36gLIZ6NFgd/1eJ
+cfxQbNA/swGWpMy4LSOrBksfZUmXqVuenwPaKe/GPh8Rc7d53Bkq5IAL+6yPTSUf
+0TIeyFBDq81s4yP89jysdoeM6VSOlAkwd40lVhOjR9mmxCeqZ/7apM/Ip7WuwYLO
+ka8jdrZ3PSj4eacoFttpo4m4YnCWp1KcUZCVUKsrKcCB8/Lg5jCWv1T1UYxeWxWT
+LoP6TExvhnuP9aSiV0uEW1d8a2+WPn3L1GtHo0vH/eHocYPs3YyPHOuxHw6QmZ6y
+8dPKkNMkvQW4Q+BUYCmKnVW4yQ98cJCISrfitbbhEu8=
 -----END CERTIFICATE-----
 EOL
 
